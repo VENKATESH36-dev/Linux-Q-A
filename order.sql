f62f9a154d7011f2ab84ac826d5bb699b24733d2
venkatesh.chennuri36@gmail.com <venkatesh.c@apexon.com>
2024-12-11 14:09:07 +0530

    new queries are added

--- order.sql
+++ order.sql
@@ -36,3 +36,38 @@ SELECT project_id, employee_id
 FROM employees_projects_temp;
 
 DROP TABLE employees_projects_temp;
+
+
+---------------------------------------------------------------------------------------
+
+--In Employees_Projects table some rows reference employees who no longer exist in the employees table. Remove these orphaned rows
+
+DELETE ep
+FROM employees_projects ep
+LEFT JOIN employees e ON ep.employee_id = e.id
+WHERE e.id IS NULL;
+
+
+--Detect Salary gaps
+--hint: you want to compare the average salary between departments.
+WITH DepartmentAvgSalary AS (
+    SELECT department_id, AVG(salary) AS avg_salary
+    FROM employees
+    GROUP BY department_id
+),
+OverallAvgSalary AS (
+    SELECT AVG(salary) AS overall_avg_salary
+    FROM employees
+)
+SELECT das.department_id, das.avg_salary, oas.overall_avg_salary,
+       (das.avg_salary - oas.overall_avg_salary) AS salary_gap
+FROM DepartmentAvgSalary das
+CROSS JOIN OverallAvgSalary oas
+ORDER BY salary_gap DESC;
+
+--Employees working on more than 1 project
+
+SELECT employee_id
+FROM employees_projects
+GROUP BY employee_id
+HAVING COUNT(DISTINCT project_id) > 1;
